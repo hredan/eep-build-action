5641c200c999870c19f5fc5afed8d74665f56b74
hredan <hredan@sourcecode3d.de>
2024-07-04 05:40:06 +0200

    try run 02

--- start_action_with_param.sh
+++ start_action_with_param.sh
@@ -5,7 +5,7 @@ param=""
 
 # -s ${{ inputs.sketch-name }} -c ${{ inputs.core }} -b ${{ inputs.board }} -f ${{ inputs.cpu-frequency }} -v ${{ inputs.core-version }} -l ${{ inputs.libs }}
 
-if [[ "$inputs" =~ ^-s (.*?) -c (.*?)]]; then
+if [[ "$inputs" =~ ^-s (.*?) -c (.*?) ]]; then
     name=${BASH_REMATCH[1]}
     echo "name: $name"
     core=${BASH_REMATCH[2]}
