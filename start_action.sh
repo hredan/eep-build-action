0173a42b4c94efae9f8fa58d6ed84a88aeb82293
hredan <hredan@sourcecode3d.de>
2024-07-04 08:02:18 +0200

    show all parameter

--- start_action.sh
+++ start_action.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-echo $INPUT_SKETCH_NAME
-echo $INPUT_CORE
-echo $INPUT_BOARD
+echo Name: $INPUT_SKETCH_NAME
+echo Core: $INPUT_CORE
+echo Board: $INPUT_BOARD
+echo CPU_f:$INPUT_CPUF
+echo CORE Version: $INPUT_CORE_VERSION
+echo LIBS: $INPUT_LIBS
