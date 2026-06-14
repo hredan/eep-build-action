4e9c50c802c2133d877eccabb9dbb8e33b198111
hredan <hredan@sleepuino.de>
2026-06-14 13:22:15 +0000

    pylint: fix errors

--- build_sketch.py
+++ build_sketch.py
@@ -44,10 +44,14 @@ if __name__ == "__main__":
     helper.install_core(build_config.core or "", build_config.core_version)
     helper.install_libs(build_config.libs or "")
 
+    fqbn_para = ""
     if build_config.core == "esp32":
-        fqbn_para = f"esp32:esp32:{build_config.board}:FlashFreq=80,PartitionScheme=default,CPUFreq={build_config.cpu_f},UploadSpeed=921600"
+        fqbn_para = f"esp32:esp32:{build_config.board}:FlashFreq=80,PartitionScheme=default" +\
+            f",CPUFreq={build_config.cpu_f},UploadSpeed=921600"
     else:
-        fqbn_para = f"esp8266:esp8266:{build_config.board}:xtal={build_config.cpu_f},vt=flash,exception=disabled,stacksmash=disabled,ssl=all,mmu=3232,non32xfer=fast,eesz=4M2M,ip=hb2f,dbg=Disabled,lvl=None____,wipe=none,baud=921600"
+        fqbn_para = f"esp8266:esp8266:{build_config.board}:xtal={build_config.cpu_f}" + \
+            ",vt=flash,exception=disabled,stacksmash=disabled,ssl=all,mmu=3232,non32xfer=fast,eesz=4M2M," + \
+            "ip=hb2f,dbg=Disabled,lvl=None____,wipe=none,baud=921600"
     BUILD_PATH = f"./BIN_{build_config.core}_{build_config.board}"
     helper.compile_sketch(build_config.sketch_name or "",
                           BUILD_PATH, fqbn_para, cpu_freq=build_config.cpu_f)
